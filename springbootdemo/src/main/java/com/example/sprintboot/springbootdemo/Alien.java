a622a7b3d791482c61c80c27349a0d0b0539ced5
Ravi <ravikumarjha059@gmail.com>
2025-08-17 18:33:36 +0530

    springboot init

---
+++ springbootdemo/src/main/java/com/example/sprintboot/springbootdemo/Alien.java
@@ -0,0 +1,11 @@
+package com.example.sprintboot.springbootdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Alien {
+    
+    public void code(){
+        System.out.println("Coding");
+    }
+}
