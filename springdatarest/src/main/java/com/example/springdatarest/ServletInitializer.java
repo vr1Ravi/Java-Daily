615020a9cfdb7130f8adcdc00c2440ca7f6b123f
Ravi <ravikumarjha059@gmail.com>
2025-08-21 22:26:35 +0530

    springdatarest init

---
+++ springdatarest/src/main/java/com/example/springdatarest/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.springdatarest;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringdatarestApplication.class);
+	}
+
+}
---
+++ springdatarest/src/test/java/com/example/springdatarest/SpringdatarestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdatarestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
